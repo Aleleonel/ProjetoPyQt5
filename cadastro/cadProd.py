af7f3d5a40c285410eb9deeca0007092dc21247d
Aleleonel <aleleonel@gmail.com>
2021-03-07 15:57:56 -0300

    estudando os componentes em um controle de caixa

--- cadastro/cadProd.py
+++ cadastro/cadProd.py
@@ -1,75 +1,15 @@
 from PyQt5 import uic, QtWidgets
-import mysql.connector
+from PyQt5.QtWidgets import QMessageBox
+
+import conexao
+
 from reportlab.pdfgen import canvas
 
-banco = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd="03120808",
-    database="cadastro_produtos"
-)
-
-
-def edit_dados():
-    linha = formulario_listprod.tableWidget.currentRow()
-
-    cursor = banco.cursor()
-    comando_SQL = "SELECT id FROM produtos"
-    cursor.execute(comando_SQL)
-    dados_lidos = cursor.fetchall()
-    valor_id = dados_lidos[linha][0]
-
-    cursor.execute("SELECT * FROM produtos WHERE id =" + str(valor_id))
-    produto = cursor.fetchall()
-    formulario_editprod.show()
-
-    formulario_editprod.ld_id.setText(str(produto[0][0]))
-    formulario_editprod.ld_codigo.setText(str(produto[0][1]))
-    formulario_editprod.ld_produto.setText(str(produto[0][2]))
-    formulario_editprod.ld_preco.setText(str(produto[0][3]))
-    formulario_editprod.ld_categoria.setText(str(produto[0][4]))
-
-
-def salvar():
-    linha = formulario_listprod.tableWidget.currentRow()
-
-    cursor = banco.cursor()
-    comando_SQL = "SELECT id FROM produtos"
-    cursor.execute(comando_SQL)
-
-    dados_lidos = cursor.fetchall()
-
-    valor_id = dados_lidos[linha][0]
-
-    cursor.execute("SELECT * FROM produtos WHERE id =" + str(valor_id))
-    produto = cursor.fetchall()
-    prod = formulario_editprod.ld_produto.text()
-    prec = formulario_editprod.ld_preco.text()
-    cat = formulario_editprod.ld_categoria.text()
-
-    comando_SQL = "UPDATE produtos SET descricao = '" + prod + "', preco = '" \
-                  + prec + "', categoria = '" + cat + "' WHERE id =" + str(valor_id)
-    cursor.execute(comando_SQL)
-    #
-    banco.commit()
-    formulario_editprod.close()
-
-
-def excluir_dados():
-    linha = formulario_listprod.tableWidget.currentRow()
-    formulario_listprod.tableWidget.removeRow(linha)
-
-    cursor = banco.cursor()
-    comando_SQL = "SELECT id FROM produtos"
-    cursor.execute(comando_SQL)
-    dados_lidos = cursor.fetchall()
-    valor_id = dados_lidos[linha][0]
-    cursor.execute("DELETE FROM produtos WHERE id =" + str(valor_id))
-    banco.commit()
+valor = 0
 
 
 def gerar_pdf():
-    cursor = banco.cursor()
+    cursor = conexao.banco.cursor()
     comando_SQL = "SELECT * FROM produtos"
     cursor.execute(comando_SQL)
     dados_lidos = cursor.fetchall()
@@ -94,10 +34,148 @@ def gerar_pdf():
         pdf.drawString(410, 750 - y, str(dados_lidos[i][4]))
 
     pdf.save()
-    print("pdf foi salvo com sucesso!")
+    global valor
+    if not menu.progressBar.isVisible():
+        menu.progressBar.show()
+        while valor <= 100:
+            valor += 0.0001
+            menu.progressBar.setValue(valor)
+    menu.progressBar.hide()
+    valor = 0
+
+    msg = QMessageBox()
+    msg.setWindowTitle("Atenção")
+    msg.setText("PDF Criado com Sucesso")
+    msg.setIcon(QMessageBox.Information)
+    msg.setDetailedText("Arquivo criado em meus documentos ")
+
+    x = msg.exec_()
 
 
-def principal():
+def edit_dados():
+    linha = formulario_listprod.tableWidget.currentRow()
+
+    cursor = conexao.banco.cursor()
+    comando_SQL = "SELECT id FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+
+    cursor.execute("SELECT * FROM produtos WHERE id =" + str(valor_id))
+    produto = cursor.fetchall()
+    formulario_editprod.show()
+
+    formulario_editprod.ld_id.setText(str(produto[0][0]))
+    formulario_editprod.ld_codigo.setText(str(produto[0][1]))
+    formulario_editprod.ld_produto.setText(str(produto[0][2]))
+    formulario_editprod.ld_preco.setText(str(produto[0][3]))
+    formulario_editprod.ld_categoria.setText(str(produto[0][4]))
+
+
+def salvar():
+    linha = formulario_listprod.tableWidget.currentRow()
+
+    cursor = conexao.banco.cursor()
+    comando_SQL = "SELECT id FROM produtos"
+    cursor.execute(comando_SQL)
+
+    dados_lidos = cursor.fetchall()
+
+    valor_id = dados_lidos[linha][0]
+
+    cursor.execute("SELECT * FROM produtos WHERE id =" + str(valor_id))
+    produto = cursor.fetchall()
+    prod = formulario_editprod.ld_produto.text()
+    prec = formulario_editprod.ld_preco.text()
+    cat = formulario_editprod.ld_categoria.text()
+
+    comando_SQL = "UPDATE produtos SET descricao = '" + prod + "', preco = '" \
+                  + prec + "', categoria = '" + cat + "' WHERE id =" + str(valor_id)
+    cursor.execute(comando_SQL)
+    #
+    conexao.banco.commit()
+    formulario_editprod.close()
+
+
+def excluir_dados():
+    linha = formulario_listprod.tableWidget.currentRow()
+    formulario_listprod.tableWidget.removeRow(linha)
+
+    cursor = conexao.banco.cursor()
+    comando_SQL = "SELECT id FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("DELETE FROM produtos WHERE id =" + str(valor_id))
+    conexao.banco.commit()
+
+
+def mostracadprod():
+    formulario.show()
+
+
+def selecionaProduto():
+    formulario_caixa.show()
+    formulario_caixa.comboBox.addItems(["Pedido", "Balcão", "Orçamento"])
+
+
+def insereItem():
+    """
+    Selecionando produto no banco de dados e preenche o listWidget
+    com o produto e preço, --- talvez seja necessário usar outro tipo
+    de widget,
+    :return: 
+    """
+
+    # Busca no banco de dados por todos os produtos
+    # salva na variavel dados_lidos
+    cursor = conexao.banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+
+
+    cont = 0
+    l = 0
+    itens_cadastrados = []
+    produto = {}
+
+    # Estou lendo de tras para frente os dados_lidos
+    # e armazenando em uma lista, isso torna possível
+    # ler a lista posteriormete
+    # com um contador l eu consigo manster o ponteiro
+    #  apontado para um unica linha ate ler o nome do item
+    while cont <= (len(dados_lidos) - 1):
+        itens_cadastrados.append(dados_lidos[l][2])
+        l += 1
+        cont += 1
+
+    # Comparo o que foi digitado no lineEdit com cada item
+    # da minha lista itens_cadastrado e salvo em um dicionário
+    # desta form eu posso manipular chave e valor de cada item
+    for idex_produto_cadastrados in itens_cadastrados:
+        if formulario_caixa.ProdutoItem.text() == idex_produto_cadastrados:
+            # armazenado o item
+            produto["item"] = idex_produto_cadastrados
+            print(idex_produto_cadastrados)
+
+            # armazenando o preço
+            for i in range(len(dados_lidos)):
+                if dados_lidos[i][2] == idex_produto_cadastrados:
+                    produto["preço"] = dados_lidos[i][3]
+                    print(dados_lidos[i][3])
+
+            # pego os itens do meu dicionario
+            # e insere o valor de cada chave listWedget
+            for k, v in produto.items():
+                prod = str(v)
+                formulario_caixa.lwItens.addItem(prod)
+
+                print(prod)
+
+
+
+def cadastroProduto():
     codigo = formulario.ldCod.text()
     descricao = formulario.ldDesc.text()
     preco = formulario.ldPrec.text()
@@ -119,15 +197,11 @@ def principal():
         print("Categoria Cabelo foi Selecionada")
         Categoria = "Cabelo"
 
-    print('Cod: ', codigo)
-    print('Descrição: ', descricao)
-    print('Preço: ', preco)
-
-    cursor = banco.cursor()
+    cursor = conexao.banco.cursor()
     comando_SQL = "INSERT INTO produtos (codigo, descricao,preco,categoria) values (%s, %s, %s, %s)"
     dados = (str(codigo), str(descricao), str(preco), Categoria)
     cursor.execute(comando_SQL, dados)
-    banco.commit()
+    conexao.banco.commit()
     # limpando todos os campos
     formulario.ldCod.setText("")
     formulario.ldDesc.setText("")
@@ -137,7 +211,7 @@ def principal():
 def listaProd():
     formulario_listprod.show()
 
-    cursor = banco.cursor()
+    cursor = conexao.banco.cursor()
     comando_SQL = "SELECT * FROM produtos"
     cursor.execute(comando_SQL)
     dados_lidos = cursor.fetchall()
@@ -151,16 +225,27 @@ def listaProd():
 
 
 app = QtWidgets.QApplication([])
+
+menu = uic.loadUi("menuPrincipal.ui")
 formulario = uic.loadUi("cadProd.ui")
 formulario_listprod = uic.loadUi("listprod.ui")
 formulario_editprod = uic.loadUi("editprod.ui")
+formulario_caixa = uic.loadUi("ctrlCaixa.ui")
 
-formulario.btnCadastrar.clicked.connect(principal)
+menu.actionCadastrar.triggered.connect(mostracadprod)
+menu.actionListProd.triggered.connect(listaProd)
+menu.actionGeraPdf.triggered.connect(gerar_pdf)
+menu.actioncaixa.triggered.connect(selecionaProduto)
+
+formulario.btnCadastrar.clicked.connect(cadastroProduto)
 formulario.btnListar.clicked.connect(listaProd)
 formulario_listprod.btn_pdf.clicked.connect(gerar_pdf)
+
 formulario_listprod.btnDel.clicked.connect(excluir_dados)
 formulario_listprod.btnEditar.clicked.connect(edit_dados)
 formulario_editprod.btnSalvarEdit.clicked.connect(salvar)
+formulario_caixa.btnInsere.clicked.connect(insereItem)
 
-formulario.show()
+menu.show()
+menu.progressBar.hide()
 app.exec()
